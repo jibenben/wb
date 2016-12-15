d5f175ab413c0b79e0ade25d9a1d0653e79ef352
Liu Jinfeng <njusunrise@163.com>
2016-12-15 23:36:34 +0800

    Make the usage of comment service more clear.

--- src/main/java/cn/edu/bjtu/weibo/service/CommentMessageService.java
+++ src/main/java/cn/edu/bjtu/weibo/service/CommentMessageService.java
@@ -1,7 +1,30 @@
 package cn.edu.bjtu.weibo.service;
 
-import cn.edu.bjtu.weibo.model.Comment;
+/**
+ * When some user try to comment some words to weibo or comment
+ * 
+ * the action will include
+ * 1 create the comment under commentList in db
+ * 2 add this commentId under weibo or comment list, it depends on the place you comment on.
+ * 3 increase the number of commentNumber.
+ * 4 this comment is belong to you, so it will be added to your BaseContent list.
+ * 
+ * so we need some parameter
+ * 1 who comments
+ * 2 the comment entity
+ * 3 which BaseContent will be attached by this comment.
+ * 
+ * @author Liu Jinfeng
+ *
+ */
 
 public interface CommentMessageService {
-	public boolean CommentMessage(Comment comment,Comment comment1);
+	/**
+	 * 
+	 * @param userId
+	 * @param commentId
+	 * @param baseContentId : can be weibo or comment
+	 * @return
+	 */
+	public boolean CommentMessage(int userId, int commentId, int baseContentId);
 }
