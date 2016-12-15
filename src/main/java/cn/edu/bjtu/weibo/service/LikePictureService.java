a43ff743a2e9a9fbbdd7b61063c5abe3ce2b6c9b
jiwawa <14301144>
2016-12-15 13:44:55 +0800

    LikePictureService

--- src/main/java/cn/edu/bjtu/weibo/service/LikePictureService.java
+++ src/main/java/cn/edu/bjtu/weibo/service/LikePictureService.java
@@ -2,8 +2,15 @@ package cn.edu.bjtu.weibo.service;
 
 import cn.edu.bjtu.weibo.model.Picture;
 import cn.edu.bjtu.weibo.model.User;
-
+/*
+when user likes a picture
+*/
 public interface LikePictureService {
-	public boolean LikePicture(User user ,Picture picture);
+	/*
+	userId
+	pictureurl
+	
+	*/
+	public boolean LikePicture(int userId ,String picurl);
 
 }
