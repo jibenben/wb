3ab3c339d02a52687a8e914ead124e91006e1388
Liu Jinfeng <njusunrise@163.com>
2016-12-15 09:15:54 +0800

    no important change

--- src/main/java/cn/edu/bjtu/weibo/service/RegisterService.java
+++ src/main/java/cn/edu/bjtu/weibo/service/RegisterService.java
@@ -1,5 +1,5 @@
 package cn.edu.bjtu.weibo.service;
 
 public interface RegisterService {
-	public Boolean registerNewUser(String username, String password);
+	Boolean registerNewUser(String username, String password);
 }
