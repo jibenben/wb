1ec64e7a6c7e6840df4fcb66badbe5634c0f5d5e
jiwawa <14301144>
2016-12-15 14:43:00 +0800

    Relation

--- src/main/java/cn/edu/bjtu/weibo/service/RelationService.java
+++ src/main/java/cn/edu/bjtu/weibo/service/RelationService.java
@@ -2,9 +2,13 @@ package cn.edu.bjtu.weibo.service;
 
 import cn.edu.bjtu.weibo.model.User;
 
+/*
+follow a user
+unfollow user
+deletefans
+*/
 public interface RelationService {
-	public boolean	followAction(User user1,User user2);
-	public boolean	unfollowAction(User user1,User user2);
-	public boolean	deletefansAction(User user1,User user2);
-
+	 boolean	followAction(int userId,int userIdfollowed);
+	 boolean	unfollowAction(int userId,int userIdunfollowed);
+	 boolean	deletefansAction(int userId,int userIddelete);
 }
