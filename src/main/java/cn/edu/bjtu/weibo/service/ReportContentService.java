9bce8481c88658f7d316f478e5d5d2321c62700a
Liu Jinfeng <njusunrise@163.com>
2016-12-16 00:19:08 +0800

    rename service and use basecontent to extend the function of report weibo or comment service.

---
+++ src/main/java/cn/edu/bjtu/weibo/service/ReportContentService.java
@@ -0,0 +1,17 @@
+package cn.edu.bjtu.weibo.service;
+
+/**
+ * When some weibo or comment contain illegal or unreal content, you can use this service to report this weibo to Admin
+ * 
+ * @author Liu Jinfeng
+ *
+ */
+public interface ReportContentService {
+	/**
+	 * 
+	 * @param userId
+	 * @param weiboId
+	 * @return
+	 */
+	boolean reportWeiboOrComment(int userId,int weiboOrCommentId);
+}
