75dc11047dc87d421d3433734c39d5340493371e
Liu Jinfeng <njusunrise@163.com>
2016-12-15 08:25:52 +0800

    update banuserservice`s comments.

--- src/main/java/cn/edu/bjtu/weibo/service/BanUserService.java
+++ src/main/java/cn/edu/bjtu/weibo/service/BanUserService.java
@@ -1,7 +1,25 @@
 package cn.edu.bjtu.weibo.service;
 
-import cn.edu.bjtu.weibo.model.User;
-
+/**
+ * When one user A ban another user B, then the user won`t see any weibo from B
+ * here, ban means the ban action
+ * 
+ * @author Liu Jinfeng
+ *
+ */
 public interface BanUserService {
-	public boolean BanUser(User user,User user1);
+	/**
+	 * The action when userId ban banedUserId
+	 * @param userId
+	 * @param banedUserId
+	 * @return
+	 */
+	boolean banUser(int userId, int banedUserId);
+	/**
+	 * The action when userId unban banedUserId
+	 * @param userId
+	 * @param banedUserId
+	 * @return
+	 */
+	boolean unBanUser(int userId, int banedUserId);
 }
