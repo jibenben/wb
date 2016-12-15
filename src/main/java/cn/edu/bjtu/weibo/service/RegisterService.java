d45108c65f5f27133d129e2fedfd16912a375b1e
jiwawa <14301144>
2016-12-15 14:38:47 +0800

    register

--- src/main/java/cn/edu/bjtu/weibo/service/RegisterService.java
+++ src/main/java/cn/edu/bjtu/weibo/service/RegisterService.java
@@ -1,5 +1,12 @@
 package cn.edu.bjtu.weibo.service;
 
+/*
+Register  a user
+*/
 public interface RegisterService {
+	/*
+	username
+	password
+	*/
 	Boolean registerNewUser(String username, String password);
 }
