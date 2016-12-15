29bea4af1cc4932deece168b24c261609fe5ffc6
jiwawa <14301144>
2016-12-15 15:10:06 +0800

    SearchWeiboContent

--- src/main/java/cn/edu/bjtu/weibo/service/SearchWeiboContentService.java
+++ src/main/java/cn/edu/bjtu/weibo/service/SearchWeiboContentService.java
@@ -1,5 +1,11 @@
 package cn.edu.bjtu.weibo.service;
 
+/**
+ * when you want to new the weibo content
+ */
 public interface SearchWeiboContentService {
-
+	/**
+	 * para weiboId
+	 */
+	String getWeiboContent(int weiboId);
 }
