34edc4e7a73a43edf7c4f0f19d894a628e3e4b64
Liu Jinfeng <njusunrise@163.com>
2016-12-15 08:19:39 +0800

    Update WeiboListService to make it support pagination

--- src/main/java/cn/edu/bjtu/weibo/service/WeiboListService.java
+++ src/main/java/cn/edu/bjtu/weibo/service/WeiboListService.java
@@ -2,10 +2,35 @@ package cn.edu.bjtu.weibo.service;
 
 import java.util.List;
 
-import cn.edu.bjtu.weibo.model.User;
 import cn.edu.bjtu.weibo.model.Weibo;
 
+/**
+ * This service work for listing weibos for user`s reading
+ * 
+ * @author Liu Jinfeng
+ */
+
 public interface WeiboListService {
-	public List<Weibo> WeiboList(User user);
-	public List<Weibo> WeiboList();
+	/**
+	 * When a userId is specified, it will return the list of weibos
+	 * and it needs pagination
+	 * 
+	 * @param userId
+	 * @param pageIndex : start from 0 which means the first page.
+	 * @param numberPerPage : for example, 20 or 30 weibos for each page.
+	 * @return
+	 */
+	List<Weibo> getWeiboList(int userId, int pageIndex, int numberPerPage);
+	
+	/**
+	 * When a userId is not specified, it will return the random list of weibos
+	 * it depends on the algorithm you think out.
+	 * and it needs pagination
+	 * 
+	 * @param userId
+	 * @param pageIndex : start from 0 which means the first page.
+	 * @param numberPerPage : for example, 20 or 30 weibos for each page.
+	 * @return
+	 */
+	List<Weibo> getWeiboList(int pageIndex, int numberPerPage);
 }
