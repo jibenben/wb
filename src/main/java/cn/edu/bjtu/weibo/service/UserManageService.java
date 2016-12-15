7f495bbd708db9e6c43ff346feb8e0e5315db762
Liu Jinfeng <njusunrise@163.com>
2016-12-15 15:27:11 +0800

    Admin can manage User in the backend.

---
+++ src/main/java/cn/edu/bjtu/weibo/service/UserManageService.java
@@ -0,0 +1,14 @@
+package cn.edu.bjtu.weibo.service;
+
+/**
+ * when the admin manage the user, it can ban some user or unban.
+ * 
+ */
+public interface UserManageService {
+	/**
+	 * ban a user by userid
+	 * @param userid
+	 */
+	 boolean banUser(int userId);
+	 boolean unBanUser(int userId);
+}
