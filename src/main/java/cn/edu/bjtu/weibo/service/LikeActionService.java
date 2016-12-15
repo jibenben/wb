bde36ff0c2389b6b12c24223d77804d31bb600bb
jiwawa <14301144>
2016-12-15 13:38:25 +0800

    change  likeWeiboActionService

--- src/main/java/cn/edu/bjtu/weibo/service/LikeActionService.java
+++ src/main/java/cn/edu/bjtu/weibo/service/LikeActionService.java
@@ -2,6 +2,13 @@ package cn.edu.bjtu.weibo.service;
 
 import cn.edu.bjtu.weibo.model.User;
 import cn.edu.bjtu.weibo.model.Weibo;
+/*
+uesr like a weibo Action
+*/
 public interface LikeActionService {
-	public boolean Like(User user ,Weibo weibo);
+	/*
+	userId
+	weiboId
+	*/
+	public boolean Like(int userId ,int weiboId);
 }
