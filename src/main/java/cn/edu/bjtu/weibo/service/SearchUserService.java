945a5a4576924fc7d720e97dd3f8a47cff36afdf
jiwawa <14301144>
2016-12-15 15:06:25 +0800

    SearchUser

--- src/main/java/cn/edu/bjtu/weibo/service/SearchUserService.java
+++ src/main/java/cn/edu/bjtu/weibo/service/SearchUserService.java
@@ -2,8 +2,11 @@ package cn.edu.bjtu.weibo.service;
 
 import java.util.List;
 
+/**
+ * when you search user by user's name
+ */
 import cn.edu.bjtu.weibo.model.User;
 
 public interface SearchUserService {
-	public List<User> SearchUser(String user);
+	 List<User> SearchUser(String username);
 }
