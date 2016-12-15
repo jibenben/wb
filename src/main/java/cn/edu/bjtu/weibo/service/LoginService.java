bd7e27de5d322c7ce6e1371b447645cf93d8bd75
Liu Jinfeng <njusunrise@163.com>
2016-12-15 09:11:48 +0800

    no important change for login

--- src/main/java/cn/edu/bjtu/weibo/service/LoginService.java
+++ src/main/java/cn/edu/bjtu/weibo/service/LoginService.java
@@ -1,5 +1,10 @@
 package cn.edu.bjtu.weibo.service;
 
+/**
+ * When user try to login weibo
+ * @author Liu Jinfeng
+ *
+ */
 public interface LoginService {
-	public boolean loginService(String usename, String password);
+	boolean loginService(String usename, String password);
 }
