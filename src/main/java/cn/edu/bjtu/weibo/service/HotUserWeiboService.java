abdd2e1ae2572003a51d06425ecf02fe8f853ac0
Liu Jinfeng <njusunrise@163.com>
2016-12-18 02:02:18 +0800

    Add springframework-web dependency to make this project can handle upload picture easily.
    modify the WeiboPictureServie comment and function to make it more easy to understand.

--- src/main/java/cn/edu/bjtu/weibo/service/HotUserWeiboService.java
+++ src/main/java/cn/edu/bjtu/weibo/service/HotUserWeiboService.java
@@ -2,7 +2,6 @@ package cn.edu.bjtu.weibo.service;
 
 import java.util.List;
 
-import cn.edu.bjtu.weibo.model.User;
 import cn.edu.bjtu.weibo.model.Weibo;
 
 /**
---
+++ src/main/java/cn/edu/bjtu/weibo/service/WeiboPictureServie.java
@@ -0,0 +1,30 @@
+package cn.edu.bjtu.weibo.service;
+
+import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
+import cn.edu.bjtu.weibo.model.Picture;
+
+/**
+ * Weibo`s pictures service.
+ * 
+ * for example, currently, one weibo have most 9 pictures with it.
+ * 
+ * @author Liu Jinfeng
+ *
+ */
+public interface WeiboPictureServie {
+	/**
+	 * Get all the pictures belong to some weibo
+	 * @param weiboId
+	 * @return
+	 */
+	List<Picture> getWeiboPictureList(String weiboId);
+	
+	/**
+	 * Upload pictures for weibo
+	 * @param weiboId
+	 * @param multipartFile
+	 * @return
+	 */
+	boolean uploadWeiboPicture(String weiboId, MultipartFile multipartFile);
+}
