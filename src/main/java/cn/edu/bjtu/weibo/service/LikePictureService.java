63ebea96c8cc7df4b199749f291a3b708477127e
Liu Jinfeng <njusunrise@163.com>
2016-12-19 22:15:30 +0800

    update LikePictureService service`s function parameter and make some comment based on my latest investigation.

--- src/main/java/cn/edu/bjtu/weibo/service/LikePictureService.java
+++ src/main/java/cn/edu/bjtu/weibo/service/LikePictureService.java
@@ -3,6 +3,12 @@ package cn.edu.bjtu.weibo.service;
 /**
  * When a user want to show his interest for some picture, he can use this service.
  * 
+ * the logic for it I need to clarify here. when some one like some picture,
+ * then the weibo which the picture belongs to will be in the user favorite weibo list,
+ * there is no favorite picture list, and for each picture, they will maintain the like number by itself,
+ * at meantime, the weibo total like number will increase, if you are in charge of implementing this service,
+ * I strongly recommend you to use weibo and do some test, then you will find hints yourself.
+ * 
  * @author Liu Jinfeng
  *
  */
@@ -13,5 +19,5 @@ public interface LikePictureService {
 	 * @param picId
 	 * @return
 	 */
-	boolean LikePictureAction(String userId ,String picId);
+	boolean LikePictureAction(String userId, String weiboId, String picId);
 }
