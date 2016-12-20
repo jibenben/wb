4cbe48183c2d64e39bfd76e791edf7dd9ef9ccac
Liu Jinfeng <njusunrise@163.com>
2016-12-20 13:39:46 +0800

    add topic and at user properties to user model.

--- src/main/java/cn/edu/bjtu/weibo/model/Weibo.java
+++ src/main/java/cn/edu/bjtu/weibo/model/Weibo.java
@@ -1,7 +1,11 @@
 package cn.edu.bjtu.weibo.model;
 
+import java.util.List;
+
 public class Weibo extends BaseContent{
 	private int forwardNumber;
+	private List<String> atUserIdList;
+	private List<String> topicIdList;
 
 	public int getForwardNumber() {
 		return forwardNumber;
@@ -9,6 +13,22 @@ public class Weibo extends BaseContent{
 
 	public void setForwardNumber(int forwardNumber) {
 		this.forwardNumber = forwardNumber;
+	}
+
+	public List<String> getAtUserIdList() {
+		return atUserIdList;
+	}
+
+	public void setAtUserIdList(List<String> atUserIdList) {
+		this.atUserIdList = atUserIdList;
+	}
+
+	public List<String> getTopicIdList() {
+		return topicIdList;
+	}
+
+	public void setTopicIdList(List<String> topicIdList) {
+		this.topicIdList = topicIdList;
 	}
 	
 }
