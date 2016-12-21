6ecef87465049ed0abb2fb0c8cbd0b2727c16a0c
jiwawa <14301144>
2016-12-21 17:09:13 +0800

    TopicDAO

--- src/main/java/cn/edu/bjtu/weibo/dao/TopicDAO.java
+++ src/main/java/cn/edu/bjtu/weibo/dao/TopicDAO.java
@@ -3,15 +3,70 @@ package cn.edu.bjtu.weibo.dao;
 import java.util.List;
 
 import cn.edu.bjtu.weibo.model.Topic;
-
+/**
+ * this is the data about topic
+ * @author ji
+ *
+ */
 public interface TopicDAO {
-	public boolean insertNewTopic(Topic t);
 	
-	List<String> getUserList(String topicid);
-	
+	/**
+	 * insert a new topic 
+	 * @param content (the topic content)
+	 * @param UserId (the id of user who create topic)
+	 * @param date 
+	 * @param WeiboId(the id of weibo which contains the topic )
+	 * @return
+	 */
+	 boolean insertNewTopic(String content,String UserId,String date,String WeiboId);
+	/**
+	 * 
+	 * @param topicid
+	 * @param pageIndex
+	 * @param pagePerNumber
+	 * @return
+	 */
+	List<String> getAllWeibo(String topicid,int pageIndex, int pagePerNumber);
+	/**
+	 * 
+	 * @param topicid
+	 * @param pageIndex
+	 * @param pagePerNumber
+	 * @return
+	 */
+	List<String> getAllComment(String topicid,int pageIndex, int pagePerNumber);
+	/**
+	 * 
+	 * @param pageIndex
+	 * @param pagePerNumber
+	 * @return
+	 */
+	List<String> getAllTopic(int pageIndex,int pagePerNumber);
+	/**
+	 * 
+	 * @param topicid
+	 * @return
+	 */
 	String getContent(String topicid);
-	String getTopicUserNumber(String topicid);
-	String getOwner(String topicid);
-	String getTine(String topicid);
+	/**
+	 * 
+	 * @param topicid
+	 * @param weiboId
+	 * @return
+	 */
+	boolean insertWeibo(String topicid,String weiboId);
+	/**
+	 * 
+	 * @param topicid
+	 * @param commentid
+	 * @return
+	 */
+	boolean insertComment(String topicid,String commentid);
+	/**
+	 * 
+	 * @param topicid
+	 * @return
+	 */
+	int getHotOfTopic(String topicid);
 }
  
