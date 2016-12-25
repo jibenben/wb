10a023652eebf8cef06d46122877930a2f3be6f3
Liu Jinfeng <njusunrise@163.com>
2016-12-25 17:39:39 +0800

    add a new function to relationmanage

--- src/main/java/cn/edu/bjtu/weibo/service/RelationManageService.java
+++ src/main/java/cn/edu/bjtu/weibo/service/RelationManageService.java
@@ -1,5 +1,7 @@
 package cn.edu.bjtu.weibo.service;
 
+import cn.edu.bjtu.weibo.model.RelationType;
+
 /**
  * Manage your relation with other users
  * there are 3 actions you can do
@@ -33,4 +35,19 @@ public interface RelationManageService {
 	 * @return
 	 */
 	boolean removeFollowerAction(String userId, String removedUserId);
+	
+	/**
+	 * given a userId, try to find what is the relation of targetUser for him
+	 * so here, A is user, B is targetUser
+	 * when a user login his account, A is himself, B can be any one he want to find relation with him
+	 * 4 types,
+	 * follower, B is the follower of A, means B is a fans of A
+	 * following, B is the following of A, means A is a fans of B, A is interest in B
+	 * ff, A follow B and B follow A.
+	 * NO, just means no relation
+	 * @param userId
+	 * @param targetUserId
+	 * @return
+	 */
+	RelationType getUserRelationType(String userId, String targetUserId);
 }
