7214ef55337931b463cd0427ad16f6a97f0a9e3a
jiwawa <14301144>
2016-12-25 09:15:23 +0800

    picture

--- src/main/java/cn/edu/bjtu/weibo/model/Picture.java
+++ src/main/java/cn/edu/bjtu/weibo/model/Picture.java
@@ -1,13 +1,22 @@
 package cn.edu.bjtu.weibo.model;
 
 public class Picture {
-	public String picurl;
+	public String picurlth;
+	public String picurlor;
+	
+	public String getPicurlor() {
+		return picurlor;
+	}
+
+	public void setPicurlor(String picurlor) {
+		this.picurlor = picurlor;
+	}
 
 	public String getPicurl() {
-		return picurl;
+		return picurlth;
 	}
 
 	public void setPicurl(String picurl) {
-		this.picurl = picurl;
+		this.picurlth = picurl;
 	}
 }
