abbb03cbfb4cad90c8ecc10201d48ce1d2f7d72c
Liu Jinfeng <njusunrise@163.com>
2016-12-26 16:52:21 +0800

    replace controller with restcontroller in other controller classes.

--- src/main/java/cn/edu/bjtu/weibo/controller/SearchController.java
+++ src/main/java/cn/edu/bjtu/weibo/controller/SearchController.java
@@ -1,9 +1,9 @@
 package cn.edu.bjtu.weibo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/s")
 public class SearchController {
 
