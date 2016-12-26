050830a7b69987bd267ffe8f59a00f757ef1fe45
Liu Jinfeng <njusunrise@163.com>
2016-12-26 16:25:11 +0800

    add some sample code for student quickly understood.
    add json support.
    you can remove it when you start to implement your self code.
    you can use /t/topic to view the result.

--- src/main/java/cn/edu/bjtu/weibo/controller/TopicController.java
+++ src/main/java/cn/edu/bjtu/weibo/controller/TopicController.java
@@ -1,10 +1,32 @@
 package cn.edu.bjtu.weibo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.edu.bjtu.weibo.model.Topic;
 
 @Controller
 @RequestMapping("/t")
 public class TopicController {
-
+	@RequestMapping(value = "/topic")
+	public @ResponseBody
+	List<Topic> getAllTopic() {
+		List<Topic> topicList = new ArrayList<Topic>();
+		Topic topic1 = new Topic();
+		topic1.setDate("2016/12/26 10:10:10");
+		topic1.setOwner("xiaoming");
+		topic1.setTopic("圣诞大战");
+		topicList.add(topic1);
+		
+		Topic topic2 = new Topic();
+		topic2.setDate("2016/12/26 12:10:12");
+		topic2.setOwner("xiaoming");
+		topic2.setTopic("2017铁路春运");
+		topicList.add(topic2);
+		return topicList;
+	}
 }
