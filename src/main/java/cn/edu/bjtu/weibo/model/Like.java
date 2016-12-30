1624784417965e3fc01bb8a3e2f9949441781753
jiwawa <14301144>
2016-12-30 22:45:34 +0800

    update like

--- src/main/java/cn/edu/bjtu/weibo/model/Like.java
+++ src/main/java/cn/edu/bjtu/weibo/model/Like.java
@@ -1,8 +1,33 @@
 package cn.edu.bjtu.weibo.model;
 
 public class Like {
-	String userId;
-	String userActionId;
-	String WeiboIdOrCommentId;
-	String time;
+	public String userId;
+	public String userActionId;
+	public String WeiboIdOrCommentId;
+	public String time;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserActionId() {
+		return userActionId;
+	}
+	public void setUserActionId(String userActionId) {
+		this.userActionId = userActionId;
+	}
+	public String getWeiboIdOrCommentId() {
+		return WeiboIdOrCommentId;
+	}
+	public void setWeiboIdOrCommentId(String weiboIdOrCommentId) {
+		WeiboIdOrCommentId = weiboIdOrCommentId;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
 }
